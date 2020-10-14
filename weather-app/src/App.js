9e4b49028e97be6b840a256551d624d9f4603422
Belaynesh <C_Hailu.Belaynesh@Cerner.com>
2020-10-14 16:25:54 -0500

    Practice-Weather-App: fetch my own github repso and display as a list

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import Forecast from './components/Forecast/Forecast';
-import Logo from './components/Logo/Logo';
-import Conditions from './components/Conditions/Conditions';
-import Forms from './components/Forms/Forms';
+import React, { useEffect, useState} from 'react';
+// import Forecast from './components/Forecast/Forecast';
+// import Logo from './components/Logo/Logo';
+// import Conditions from './components/Conditions/Conditions';
+// import Forms from './components/Forms/Forms';
+import List from './component/List';
+import WithListLoading from './component/WithListLoading';
 import './App.css';
 
 function App() {
+  const ListLoading = WithListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+  useEffect(()=>{
+    setAppState({ loading: true});
+    const apiUrl=`https://api.github.com/users/b-hailu/repos`;
+      fetch(apiUrl)
+      .then((res) => res.json())
+      .then((repos) => {
+        setAppState({ loading: false, repos: repos });
+      });
+  }, [setAppState]);
   return (
     <div className="App">
-      <header className="App-header">
-      <Logo />
-      <h1>Practice React Weather App</h1>
-      </header>
+      <div className="container">
+      <h1>My Repositories</h1>
+      </div>
       <main>
         {/* add weather fetching component */}
         {/* <Forms  />
@@ -19,7 +35,18 @@ function App() {
         <Conditions /> */}
         {/* <Weather /> */}
       </main>
-      <footer>Page created by Belaynesh</footer>
+      <div className='repo-container'>
+      <ListLoading isLoading={appState.loading} repos={appState.repos} />
+      </div>
+      <footer>
+        <div className='footer'>
+          Built{' '}
+        <span role='img' arial-label='love'>
+
+        </span>{' '}
+        with by Bella
+        </div>
+      </footer>
     </div>
   );
 }
